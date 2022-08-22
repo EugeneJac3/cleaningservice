cbfc244625400ad076a68c2639d5e88ee575b8c3
eugenejac3 <eugene.jackson9@gmail.com>
2022-08-22 14:00:07 -0500

    get route working in server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
+const UserModel = require('./models/Users')
 require("dotenv").config();
 
 mongoose.connect(process.env.MongoURI)
+
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if(err){
+            res.json(err)
+        } else {
+            res.json(result)
+        }
+    })
+})
 
 
 app.listen(3001, () => {
